d661f9eb58f94175d735cee9a450f50764e698dc
shashi <shashi2k6@gmail.com>
2021-01-17 18:07:03 -0500

    added code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {  
+  
   return (
-    <div>
+    <div>   
     <h1>Shopping List for :</h1>
     <ul>
       <li>Instagram</li>
       <li>WhatsApp</li>
       <li>Oculus</li>
     </ul>
+    
+    <Folder/>
   </div>
+  
   );
 }
+const Folder =()=>{
+  return <h1>Folder Content</h1>
+}
+
 
 export default App;
